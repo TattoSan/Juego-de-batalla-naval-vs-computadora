0add5dc4c2aa9cb3d84cda8813564621cdf90f8d
TattoSan <31145529+TattoSan@users.noreply.github.com>
2017-10-22 01:44:44 -0500

    Update numeros aleatorios.cpp

--- numeros aleatorios.cpp
+++ numeros aleatorios.cpp
@@ -11,9 +11,9 @@
 
 int verifica( int aleatorio[][10] , int col , int fil , int num ) //función para evitar que se repita el valor en la matriz
 {
-	for( col = 0 ; col < 10 ; col++ )
+	for( fil = 0 ; fil < 10 ; fil++ )
 	{
-		for( fil = 0 ; fil < 10 ; fil++ )
+		for( col = 0 ; col < 10 ; col++ )
 		{
 			if( num == aleatorio[col][fil] )
 			{
@@ -23,15 +23,16 @@ int verifica( int aleatorio[][10] , int col , int fil , int num ) //función par
 	}
 	return verdadero;
 }
+
 int main()
 {
 	
 	int aleatorio[10][10];
-	int col , fil , num;
+	int col , fil , num,contador=0;
 	srand(time(NULL));
-	for( col = 0 ; col < 10 ; col ++)
+	for( fil = 0 ; fil < 10 ; fil ++)
 	{
-		for( fil = 0 ; fil < 10 ; fil++ )
+		for( col = 0 ; col < 10 ; col++ )
 		{
 			num = 1 + rand()%100;
 			while( verifica( aleatorio , 10 , 10 , num ) == falso)
@@ -41,13 +42,28 @@ int main()
 			aleatorio[col][fil] = num;
 		}
 	}
-	for( col = 0 ; col < 10 ; col++ )
+	for( fil = 0 ; fil < 10 ; fil++ )
 	{
 		printf("\n");
-		for( fil = 0 ; fil < 10 ; fil++ )
+		for( col = 0 ; col < 10 ; col++ )
 		{
 			printf("\t%d",aleatorio[col][fil]);
 		}
+	}
+	while(contador<=100)
+	{
+		contador++;
+		for( fil = 0 ; fil < 10 ; fil++ )
+		{
+			
+			for( col = 0 ; col < 10 ; col++ )
+			{
+				if(contador==aleatorio[col][fil])
+				{
+					printf("\nTurno [%d] -> Columna[%d] , Fila[%d]",contador,col+1,fil+1);
+				}
+			}
+	}	
 	}
 		return 0;
 }
